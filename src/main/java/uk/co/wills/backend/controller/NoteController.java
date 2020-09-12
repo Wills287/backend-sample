821ccf8cabecd3d539334fc2a002ea7f382678c5
Mark Williams <Mark.Williams@informa.com>
2020-09-12 14:54:28 +0100

    feat: add update method

--- src/main/java/uk/co/wills/backend/controller/NoteController.java
+++ src/main/java/uk/co/wills/backend/controller/NoteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,6 +59,22 @@ public class NoteController {
         return ResponseEntity.ok(
                 noteRepository.save(note)
         );
+    }
+
+    @PutMapping(
+            value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Note> update(
+            long id,
+            @RequestBody Note note
+    ) {
+        Note existing = noteRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        existing.setTitle(note.getTitle());
+        existing.setBody(note.getBody());
+        return ResponseEntity.ok(noteRepository.save(note));
     }
 
     @DeleteMapping(
